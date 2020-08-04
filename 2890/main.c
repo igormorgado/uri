2dfb95b47e00c476f12121eaf3c97b2386064f3d
Igor Morgado <morgado.igor@gmail.com>
2020-08-04 01:17:05 -0300

    2890 quase

--- 2890/main.c
+++ 2890/main.c
@@ -1,29 +1,106 @@
 #include <stdio.h>
-#define MODULO 1000007
-// #define MAXN 10000
+#include <math.h>
 
-int main(void) {
-    // int fat[MAXN];
-    // fat[0] = 1;
-    // fat[1] = 1;
-    // for(size_t i = 2; i < MAXN; ++i)
-    //     fat[i] = (i * fat[i-1]) % MODULO;
+typedef long int li;
 
-    int N = 0;
-    while(scanf("%d", &N) != EOF)
+#define MODULO (li)1000007
+#define MAXN 10001
+
+
+li f[MAXN];
+
+void gcde(li a, li b, li *d, li *x, li *y);
+li divm(li x, li y, li n);
+li invm(li b, li n);
+li C(li n, li p);
+li mulm(li x, li y, li n);
+li powm(li x, li n);
+void precomputa_fatorial();
+
+
+li mulm(li x, li y, li n)
+{
+    return (x * y) % n;
+}
+
+li powm(li x, li n)
+{
+    return mulm(x, x, n);
+}
+
+li divm(li x, li y, li n)
+{
+    return ((x % n) * (invm(y, n) % n)) % n;
+}
+
+li invm(li b, li n)
+{
+    li d = 0;
+    li x = 0;
+    li y = 0;
+    gcde(b, n, &d, &x, &y);
+    return (x+n) % n;
+}
+
+void gcde(li a, li b, li *d, li *x, li *y)
+{
+    if (b == 0)
     {
-        if(N == 0) break;
-        long int res = N;
-        res *= N-1;
-         res %= MODULO;
-        res *= N-2;
-        res %= MODULO;
-        res *= N-3;
-        res %= MODULO;
-        res /= 12;
-        fprintf(stdout,"%ld\n", res);
+        *d = a;
+        *x = 1;
+        *y = 0;
+        return;
     }
 
+    gcde(b, a%b, d, x, y);
+    li nx = *x;
+    *x = *y;
+    *y = nx - (a / b) * (*y);
+}
+
+li C(li n, li p)
+{
+    if (p==0) return 1;
+    else return (li)(round((double)(C(n-1, p-1)) * ((double)n/(double)p)));
+}
+
+
+void precomputa_fatorial()
+{
+    f[0] = 1;
+    f[1] = 1;
+    for(size_t i = 2; i < MAXN; ++i)
+        f[i] = (f[i-1] * i) % MODULO;
+}
+
+
+int main(void) {
+    precomputa_fatorial();
+
+    int n = 0;
+    while(scanf("%d", &n) != EOF)
+    {
+        if(n == 0) break;
+
+        // a(n) = C(n,1) + 3C(n,2) + 3C(n,3) + 2C(n,4)
+        //li res = 0;
+        // li p = 0;
+        // p = 1;
+        // res +=     divm(f[n], (f[p] * f[n-p] % MODULO), MODULO);
+        // p = 2;
+        // res += 3 * divm(f[n], (f[p] * f[n-p] % MODULO), MODULO);
+        // p = 3;
+        // res += 3 * divm(f[n], (f[p] * f[n-p] % MODULO), MODULO);
+        // p = 4;
+        // res += 2 * divm(f[n], (f[p] * f[n-p] % MODULO), MODULO);
+
+        /* FASTER! */
+        li p = powm(n, MODULO);
+        li pp = powm(p, MODULO);
+        li res = divm(((((11 * p) % MODULO) + pp) % MODULO), 12, MODULO);
+
+        fprintf(stdout,"%ld\n", res);
+    }
 
     return 0;
 }
